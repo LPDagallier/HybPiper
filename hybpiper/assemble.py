cb316731dad08aaa3e152a34f8e95e97d13afa49
chrisjackson <chris.jackson@rbg.vic.gov.au>
2022-07-19 16:49:56 +1000

    fix_targetfile.py W.I.P

--- hybpiper/assemble.py
+++ hybpiper/assemble.py
@@ -1699,7 +1699,7 @@ def parse_arguments():
     group_1.add_argument('--version', '-v',
                          dest='version',
                          action='version',
-                         version='%(prog)s 2.0.1rc build 12',
+                         version='%(prog)s 2.0.1rc build 12 dev',
                          help='Print the HybPiper version number.')
 
     # Add subparsers:
